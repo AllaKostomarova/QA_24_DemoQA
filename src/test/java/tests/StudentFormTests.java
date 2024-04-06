490e63be61647a01f16dfa134835275f2f0a25d0
Alla Kostomarova <alla.kostomarova@mail.ru>
2024-04-06 15:21:50 +0300

    test

--- src/test/java/tests/StudentFormTests.java
+++ src/test/java/tests/StudentFormTests.java
@@ -50,7 +50,6 @@ public class StudentFormTests extends TestBase implements HelperStudent {
         softAssert.assertTrue(isElementPresent_titleSubmitForm());
         softAssert.assertTrue(isEmailFromFormEqualsTo(student.getEmail()));
         softAssert.assertTrue(isMobileFromFormEqualsTo(student.getMobile()));
-        
     }
 
     @Test
